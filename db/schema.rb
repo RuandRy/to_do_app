900a4e37b057d52cf7401392bc2f15194430f4e2
Ru-T <rwajnberg@gmail.com>
2015-10-15 14:39:34 -0400

    Drop row order and add priority.

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015180452) do
+ActiveRecord::Schema.define(version: 20151015183733) do
 
   create_table "to_dos", force: :cascade do |t|
     t.string   "task_name"
     t.date     "due_on"
-    t.integer  "row_order"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean  "completed"
+    t.integer  "priority"
   end
 
 end
