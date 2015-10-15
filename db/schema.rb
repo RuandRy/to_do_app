c2b81e00af66bba3af2d93fe9114fec9d17b19fb
Ru-T <rwajnberg@gmail.com>
2015-10-15 14:06:40 -0400

    Add column for completed on to todo table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015180221) do
+ActiveRecord::Schema.define(version: 20151015180452) do
 
   create_table "to_dos", force: :cascade do |t|
     t.string   "task_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151015180221) do
     t.integer  "row_order"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "completed"
   end
 
 end
