32cc06df55827dcc503b572b70a8aadb0a887475
Ru-T <rwajnberg@gmail.com>
2015-10-16 11:22:21 -0400

    Create seeds for todos.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+t1 = ToDo.create!(task_name: "Visit grandma and bring her cookies", due_on: "2015-10-20", priority: 1, completed: false)
+t2 = ToDo.create!(task_name: "Mow the yard", due_on: "2015-10-25", priority: 2, completed: false)
+t3 = ToDo.create!(task_name: "Wash clothes and fold laundry", due_on: "2015-10-26", priority: 3, completed: false)
+t4 = ToDo.create!(task_name: "Cook authentic Cajun rice and beans", due_on: "2015-10-22", priority: 4, completed: false)
+t5 = ToDo.create!(task_name: "Run a half marathon", due_on: "2015-10-29", priority: 5, completed: false)
+t6 = ToDo.create!(task_name: "Call my wife and tell her I miss her", due_on: "2015-10-29", priority: 6, completed: false)
+t7 = ToDo.create!(task_name: "Build surfboard", due_on: "2015-10-30", priority: 7, completed: false)
+t8 = ToDo.create!(task_name: "Finish making Halloween costumes", due_on: "2015-10-31", priority: 8, completed: false)
