c3d26c7516512e88b9aa393577dad77488c46a9b
Ryan Graham <ryangrahamwpb@gmail.com>
2015-10-16 16:27:38 -0400

    Add destroy action to session controller.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,10 +8,13 @@ class SessionController < ApplicationController
       redirect_to to_dos_path, notice: "You have been successfully logged in!"
       session[:user_id] = u.id
     else
-      redirect_to session_new_pat, notice: "Invalid user email or password!"
-    end  
+      redirect_to session_new_path, notice: "Invalid user email or password!"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to session_new_path, notice: "You have been logged out!"
+
   end
 end
