4615866860d46a0378cd0348969db5e0090026b8
Ru-T <rwajnberg@gmail.com>
2015-10-16 09:33:45 -0400

    Give each teask in the loop a dataid and a datapos.

--- app/assets/javascripts/to_dos.js
+++ app/assets/javascripts/to_dos.js
@@ -1,11 +1,19 @@
+var ready, set_positions;
+
+set_positions = function(){
+    // loop through and give each task a data-pos
+    // attribute that holds its position in the DOM
+    $('.panel.panel-default').each(function(i){
+        $(this).attr("data-pos",i+1);
+    });
+}
+
+
 var ready;
 ready = function(){
     // call sortable on our div with the sortable class
+    set_positions();
     $('.sortable').sortable();
 }
 
 $(document).ready(ready);
-/**
- * if using turbolinks
- */
-$(document).on('page:load', ready);
