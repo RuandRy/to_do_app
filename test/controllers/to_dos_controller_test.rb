c0583efb4d7f4d0dd41283a75ae5a08223bb61a8
Ru-T <rwajnberg@gmail.com>
2015-10-18 12:57:22 -0400

    Fix to do controllers tests.

--- test/controllers/to_dos_controller_test.rb
+++ test/controllers/to_dos_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ToDosControllerTest < ActionController::TestCase
   setup do
     @to_do = to_dos(:one)
+    session[:user_id] = users(:one).id
   end
 
   test "should get index" do
